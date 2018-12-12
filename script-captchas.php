827fbd5dc32626f47ee5f924beffde050ab26741
pauline-jelic <45633051+pauline-jelic@users.noreply.github.com>
2018-12-12 16:39:46 +0100

    Update script-captchas.php

--- script-captchas.php
+++ script-captchas.php
@@ -1,16 +1,17 @@
+<meta-charset="utf-8">
 <?php
 
 /****************************************************************
 * CAPTCHA GENERATEUR IMAGE EN PHP
-* /!\ PAS DE RETOUR D'ERREUR SUR LA GÉNÉRATION
-* SCRIPT ALLEGÉ SANS OPTIMISATION POUR LA COMPRÉHENSION
+* /!\ PAS DE RETOUR D'ERREUR SUR LA GÃ‰NÃ‰RATION
+* SCRIPT ALLEGÃ‰ SANS OPTIMISATION POUR LA COMPRÃ‰HENSION
 *****************************************************************/
 
 /****************************************************************
 * 1. PARAMETRAGE DES ATTRIBUTS VARIABLES
 *****************************************************************/
-/* CHAINE DE CARACTÈRE PARAMÈTRABLE
-* SUPPRESSION DE 1 & I POUR ÉVITER LA CONFUSION DE LECTURE */
+/* CHAINE DE CARACTÃˆRE PARAMÃˆTRABLE
+* SUPPRESSION DE 1 & I POUR Ã‰VITER LA CONFUSION DE LECTURE */
 $chaine = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
 
 /* CREATION de l'image par defaut en background */
@@ -65,10 +66,10 @@ imagettftext($image, 28, -15, 120, 37, $color, $font, $char5);
 /* ENTETE HTTP A RENVOYER POUR LA GENERATION DE L'iMAGE */
 header('Content-Type: image/png');
 
-/* ENVOI DE L'IMAGE PNG GENERÉE AU NAVIGATEUR */
+/* ENVOI DE L'IMAGE PNG GENERÃ‰E AU NAVIGATEUR */
 imagepng($image);
 
-/* DESTRUCTION DE L'IMAGE LIBÉRATION DE MÉMOIRE */
+/* DESTRUCTION DE L'IMAGE LIBÃ‰RATION DE MÃ‰MOIRE */
 imagedestroy($image);
 
 ?>
