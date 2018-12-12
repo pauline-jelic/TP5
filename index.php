c6f0150354521db0ac811f112d14e5c881a5c8a7
pauline-jelic <45633051+pauline-jelic@users.noreply.github.com>
2018-12-12 16:38:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
+<meta charset="="utf-8">
 <?php
 
 /* SCRIPT FRONTAL AFFICHAGE DE CAPTCHA
-* AFFICHER UNE IMAGE GÉNÉRÉE EN PHP */
+* AFFICHER UNE IMAGE GÃ‰NÃ‰RÃ‰E EN PHP */
 
 echo "<img src='script-captchas.php' alt='captchas' />";
 
